4316a933ef1081f21333d461f15fe04f952a609d
unknown <daragopi69@gmail.com>
2024-12-15 00:31:01 +0530

    first commit

---
+++ App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+// import Home from './Home';
+// import data from'./data.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Task from './Task';
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+
+
+
+function App() {
+  return (
+   <div>
+    <Task />
+   </div>
+  );
+}
+
+export default App;
